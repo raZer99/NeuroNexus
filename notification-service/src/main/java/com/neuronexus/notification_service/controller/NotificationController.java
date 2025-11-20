d0db7bf40a029ec08076b4c896da7de8eb0a4d27
Md Sameer <mdsameer8729@gmail.com>
2025-11-20 19:53:29 +0530

    feat: add notification-service with message handler endpoint

---
+++ notification-service/src/main/java/com/neuronexus/notification_service/controller/NotificationController.java
@@ -0,0 +1,19 @@
+package com.neuronexus.notification_service.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notify")
+public class NotificationController {
+
+    @PostMapping
+    public String notifyTeam(@RequestBody String message){
+        System.out.println("Notification Service");
+        System.out.println("Message: " + message);
+        return "Notification sent successfully";
+    }
+}
