a1cd627f7522a281816866dca1b23e0545cb5b01
Md Sameer <mdsameer8729@gmail.com>
2025-11-20 19:54:53 +0530

    feat: add deployment-service with mock deployment endpoint

---
+++ deployment-service/src/main/java/com/neuronexus/deployment_service/controller/DeploymentController.java
@@ -0,0 +1,19 @@
+package com.neuronexus.deployment_service.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/deploy")
+public class DeploymentController {
+
+    @PostMapping
+    public String deploy(@RequestBody String env) {
+        System.out.println("DEPLOYMENT SERVICE:");
+        System.out.println("Deploying to: " + env);
+        return "Deployment executed for environment: " + env;
+    }
+}
